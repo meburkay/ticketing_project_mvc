72312d6950b5ce70fdc7ade079ae03e3c7414609
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-08-23 20:23:06 +0300

    injected RoleService to UserController and by finAll method we take all the roles and we put them at view select.

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.service.RoleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+    private final RoleService roleService;
+
+    public UserController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
     @GetMapping("/create")
     public String createUser(Model model){
 
         model.addAttribute("user",new UserDTO());
 
-        model.addAttribute("roles","find all roles from db"); //We must take all the roles from DB and give them to view for the dropdown button.
+        model.addAttribute("roles",roleService.findAll()); //We must take all the roles from DB and give them to view for the dropdown button.
 
         return "/user/create";
     }
