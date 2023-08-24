8bd1275c43181168ae19387bcb15826f0ea42d20
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-08-24 12:17:24 +0300

    user/update @GetMapping method added, update button at create.html implemented, create copied to update.html

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -5,10 +5,7 @@ import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
@@ -48,5 +45,15 @@ public class UserController {
 
         return "redirect:/user/create";//By redirect, we go to end point means method. Because of that we do not need the commented parts. We do the save operation and go to user/create endpoint method. This implementations already done there.
 
+    }
+
+    @GetMapping("/update/{username}")//We use here pathvariable to take the data from the UI to here. And by this information we take the user object from the database by userService.finById method. And we use it at the user/update ui html view.
+    public String editUser(@PathVariable("username") String username, Model model){
+
+        model.addAttribute("user",userService.findById(username));
+        model.addAttribute("roles",roleService.findAll());
+        model.addAttribute("users",userService.findAll());
+
+        return "/user/update";
     }
 }
