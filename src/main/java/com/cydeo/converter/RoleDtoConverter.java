f2e9130bc3ea5e963758817b4848501a5e731098
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-08-24 11:00:32 +0300

    RoleDtoConverter comment added

--- src/main/java/com/cydeo/converter/RoleDtoConverter.java
+++ src/main/java/com/cydeo/converter/RoleDtoConverter.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationPropertiesBinding
 public class RoleDtoConverter implements Converter<String, RoleDTO> {
-//When we have an object at the view and there is another object in that object we have an error about the second object. To solve this problem we use converter. This is a tyhmeleaf issue. We emplement our class with Converter like above. When the app have an issue like this: Cannot convert value of type 'java.lang.String' to required type 'com.cydeo.dto.RoleDTO' for property 'role', it automatically look for converters because of the @ConfigurationPropertiesBinding annotation and if it finds a suitable method like below it convert and solve the issue by this way.
+//When we have an object at the view and there is another object in that object we have an error about the second object. To solve this problem we use converter. This is a thymeleaf issue. We implement our class with Converter like above. When the app have an issue like this: Cannot convert value of type 'java.lang.String' to required type 'com.cydeo.dto.RoleDTO' for property 'role', it automatically look for converters because of the @ConfigurationPropertiesBinding annotation and if it finds a suitable method like below it convert and solve the issue by this way. I think the main reason is when you return a value from that object it became string you can not return object because of that according to the returning value you have to do the convert implementation here.
     RoleService roleService;
 
     public RoleDtoConverter(RoleService roleService) {
